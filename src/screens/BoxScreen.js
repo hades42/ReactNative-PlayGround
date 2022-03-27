77ff1ce79c77034b1a791da4606da9cf11711a3c
Van Nguyen Nguyen <nguyenvannguyen.oc@gmail.com>
2022-03-27 11:31:10 +1100

    Some flexbox thing

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,32 @@ import { Text, View, StyleSheet } from "react-native";
 
 const BoxScreen = () => {
     return (
-        <View>
-            <Text>Box Screen</Text>
+        <View style={styles.viewStyle}>
+            <Text style={styles.textStyleOne}>Box Screen</Text>
+            <Text style={styles.textStyle}>Box Screen</Text>
+            <Text style={styles.textStyle}>Box Screen</Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderColor: "black",
+        height: 200,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    textStyle: {
+        borderWidth: 5,
+        borderColor: "red",
+        margin: 20,
+    },
+    textStyleOne: {
+        borderWidth: 5,
+        borderColor: "blue",
+        ...StyleSheet.absoluteFillObject,
+    },
+});
 
 export default BoxScreen;
